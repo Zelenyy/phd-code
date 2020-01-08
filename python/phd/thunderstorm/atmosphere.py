268c14eed00a835410f127f164caf3c7f80153e0
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-01-08 22:00:43 +0300

    Add air composition

---
+++ python/phd/thunderstorm/atmosphere.py
@@ -0,0 +1,17 @@
+
+# Simple atmosphere composition
+air_simple = {'N2': 80, 'O2': 20}
+
+# Accuracy atmosphere composition
+air_accuracy = {
+    'N2' : 75.50733,
+    'O2' : 23.15,
+    'Ar' : 1.292,
+    'CO2' : 0.046,
+    'Ne' : 0.0014,
+    'Kr' : 0.003,
+    'CH4' : 0.000084,
+    'He' : 0.000073,
+    'H2' : 0.00008,
+    'Xe' : 0.00004
+}
