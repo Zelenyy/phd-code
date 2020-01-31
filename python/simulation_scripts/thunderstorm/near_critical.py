377b7e1829fe6958f29dda1ae41859d693abe41b
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-01-31 13:55:21 +0300

    Process diff model and near critical

--- python/simulation_scripts/thunderstorm/near_critical.py
+++ python/simulation_scripts/thunderstorm/near_critical.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import numpy as np
 from dataforge import Meta
 from phd.thunderstorm.convert_to_hdf5 import READERS_TXT, READERS_CYLINDER_DATA
 from phd.utils.hdf5_tools import get_convertor
@@ -12,6 +13,7 @@ INPUT_TEMPLATE = """/df/project test
 /df/gdml ${path}
 /thunderstorm/physics ${physics}
 /thunderstorm/stacking one_generation
+/thunderstorm/cut/energy ${cut}
 
 /gps/particle ${particle}
 /gps/number 1
@@ -30,14 +32,15 @@ def main():
     values_gdml = {
     'height' : [0],
     'cellHeight' : [600],
-    'fieldValueZ' : [0, 3e-4, 10e-4],
+    'fieldValueZ' : [7e-4],
     }
 
     values_macros = {
-    "physics" : ["standard","standard_opt_1","standard_opt_2","standard_opt_3",  "standard_opt_4", "penelopa", "livermore", "emlowepphysics"],
+    "physics" : ["standard_opt_4"],
+        "cut" : [0.05],
     'number' : [int(1000)],
-    'energy' : [10],
-    'posZ' : [299.9],
+    'energy' : np.arange(2.0, 100.1, 10),
+    'posZ' : [200],
     'direction' : ['0 0 -1'],
     'particle' : 'e-'
                   }
@@ -51,7 +54,7 @@ def main():
     input_data = general_input_generator(meta, gdml_template, INPUT_TEMPLATE)
     command = "../build/thunderstorm/geant4-thunderstorm.exe"
     readers = READERS_CYLINDER_DATA + READERS_TXT
-    multirun_command(input_data, command, post_processor=get_convertor(readers, "./result.hdf5", clear=False))
+    multirun_command(input_data, command, post_processor=get_convertor(readers, "./result.hdf5", clear=True))
     return 0
 
 if __name__ == '__main__':
