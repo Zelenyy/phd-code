abcaac70ad279b18617f42e876fef3b4917b0689
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-02-11 04:16:08 +0300

    Dwyer 2003-Update

---
+++ python/phd/thunderstorm/gurevich_length.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def calc_gurevich_len_1(data, z_cut):
+
+    data_e = data[data["particle"] == 11] # choose electrons
+    data_e = data_e[np.logical_and(data_e["z"]> -z_cut, data_e["z"]<z_cut)]
+    indx  =  np.isin(data_e["id"], data["parent_id"])
+    data_e = data_e[indx]
+
+    z = np.sort(data_e["z"])
+    Ne = np.arange(1,z.size+1)
+    def power(x, l):
+        return np.exp(x/l)
+
+    popt, pcov = curve_fit(power, z, Ne, sigma=Ne**0.5)
+    return popt, pcov
+
