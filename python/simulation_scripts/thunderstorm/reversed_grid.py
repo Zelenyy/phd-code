7d1a64f4bba9ea8a8e41809a1a2d34668581bd5f
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-02-11 05:10:00 +0300

    Dwyer 2003-Update

--- python/simulation_scripts/thunderstorm/reversed_grid.py
+++ python/simulation_scripts/thunderstorm/reversed_grid.py
@@ -1,12 +1,14 @@
 import logging
 import os
+from string import Template
 
 import numpy as np
 from dataforge import Meta
 from phd.thunderstorm.convert_to_hdf5 import get_named_cylinder_readers, get_named_number_readers_1
 from phd.utils.convertor_tools import theta_to_direction
 from phd.utils.hdf5_tools import get_convertor
-from phd.utils.run_tools import multirun_command, general_input_generator
+from phd.utils.run_tools import multirun_command, general_input_generator, create_gdml, dir_name_generator, \
+    values_from_dict, InputData
 
 ROOT_PATH = os.path.dirname(__file__)
 
@@ -26,6 +28,49 @@ INPUT_TEMPLATE = """/df/project test
 /run/beamOn ${number}
 """
 
+
+def values_macros_gen(paths):
+    for path in paths:
+        for theta in np.arange(0, 91, 10):
+            for energy in np.arange(0.1, 3.01, 0.05):
+                if energy > 2 and theta > 55:
+                    continue
+                values_macros = {
+                    "path": path,
+                    "cut": 0.05,
+                    'number': 1,
+                    'energy': energy,
+                    'direction': theta_to_direction(np.deg2rad(theta)),
+                }
+                yield values_macros
+
+def reverse_grid_input_generator(gdml_template_file: str, macros_template: str):
+    macros_template = Template(macros_template)
+    values_gdml = {
+        'height': [0],
+        'fieldValueZ': 1e-4 * np.arange(5.0, 10.1, 0.5),
+    }
+    paths, values_gdml = create_gdml(gdml_template_file, values_gdml)
+    paths = list(map(lambda x: os.path.join("..", x), paths))
+    for path, values in zip(
+            dir_name_generator(".", "sim"),
+            values_macros_gen(paths)
+    ):
+        text = macros_template.substitute(values)
+        path_gdml = values["path"]
+        indx = paths.index(path_gdml)
+        input_data_meta = {
+            "macros": values,
+            "gdml": values_gdml[indx]
+        }
+        data = InputData(
+            text=text,
+            path=path,
+            values=Meta(input_data_meta)
+        )
+        yield data
+
+
 def get_readers():
     readers = []
     txt = get_named_number_readers_1(["e-"], "particle_detector")
@@ -36,35 +81,13 @@ def get_readers():
     readers = readers + txt + binReader
     return readers
 
+
 def main():
-    logging.basicConfig(filename = "run.log")
+    logging.basicConfig(filename="run.log")
     logging.root.setLevel(logging.DEBUG)
-
     gdml_template = os.path.join(ROOT_PATH, "template", "reversed_electron.gdml")
-
-    values_gdml = {
-    'height' : [0],
-    'fieldValueZ' : 1e-4 * np.arange(5.0, 10.1, 0.5),
-    }
-
-    energy = [1] #np.arange(0.1, 1.01, 0.05)
-    theta = [0] #np.arange(0, 91, 10)
-
-    values_macros = {
-    "cut" : [0.05],
-    'number' : [int(1)],
-    'energy' : energy,
-    'direction' : list(map(theta_to_direction, np.deg2rad(theta))),
-                  }
-    meta = Meta(
-        {
-            "macros": values_macros,
-            "gdml": values_gdml
-        }
-    )
-
-    input_data = general_input_generator(meta, gdml_template, INPUT_TEMPLATE)
-    command = "../build/thunderstorm/geant4-thunderstorm.exe"
+    input_data = reverse_grid_input_generator(gdml_template, INPUT_TEMPLATE)
+    command = "../../build/thunderstorm/geant4-thunderstorm.exe"
     readers = get_readers()
     multirun_command(input_data, command, post_processor=get_convertor(readers, "./result.hdf5", clear=False))
     return 0
