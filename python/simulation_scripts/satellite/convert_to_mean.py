6328b5549d6bff870383a151523fbeb31eb5adb9
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-06-02 19:34:19 +0300

    Satellite polystyrene sim

--- python/simulation_scripts/satellite/convert_to_mean.py
+++ python/simulation_scripts/satellite/convert_to_mean.py
@@ -2,7 +2,8 @@ from phd.satellite.mean_table import convert_to_mesh
 
 
 def main():
-    convert_to_mesh("proton.hdf5", "mean_mesh.hdf5",particle="proton")
+    convert_to_mesh("./proton/proton.hdf5", "mean_mesh.hdf5",particle="proton")
+    convert_to_mesh("./electron/electron.hdf5", "mean_mesh.hdf5",particle="electron")
 
 
 if __name__ == '__main__':
