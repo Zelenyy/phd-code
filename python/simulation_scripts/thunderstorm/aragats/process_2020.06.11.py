b14e8176d68f93e5abfd49f225ec0ec32551eab2
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-07-01 16:20:00 +0300

    Aragats electron EXAPCS

--- python/simulation_scripts/thunderstorm/aragats/process_2020.06.11.py
+++ python/simulation_scripts/thunderstorm/aragats/process_2020.06.11.py
@@ -6,6 +6,11 @@ bins_1 = np.arange(0.05, 1.01, 0.05)
 bins_2 = np.arange(1, 202, 2)
 
 
+def get_values(data):
+    values_low, _ = np.histogram(data, bins=bins_1)
+    values_high, _ = np.histogram(data, bins=bins_2)
+    return values_high, values_low
+
 class HistrogramData(tables.IsDescription):
     energy = tables.Float64Col()
     number = tables.Int32Col()
@@ -31,14 +36,8 @@ def convert():
                     data = table.read()
                     indx_gamma = data["particle"] == 22
                     gamma = data[indx_gamma]
-                    def get_values(data):
-                        values_low, _ = np.histogram(data, bins=bins_1)
-                        values_high, _ = np.histogram(data, bins=bins_2)
-                        return values_high, values_low
-
                     g_hig, g_low = get_values(gamma["energy"])
                     e_hig, e_low = get_values(data[np.logical_not(indx_gamma)]["energy"])
-
                     row = hist_table.row
                     row["energy"] = energy
                     row["number"] = number
@@ -52,6 +51,36 @@ def convert():
                 hist_table.flush()
     return 0
 
+def add_1mev():
+    with tables.open_file("hist_220_expacs.hd5", "a") as fout:
+        hist_table = fout.get_node("/", "histogram")
+        row = hist_table.row
+        path = "electron_1MeV.hdf5"
+        with tables.open_file(path) as h5file:
+            for group in h5file.root:
+                table = h5file.get_node(group, "particle_detector")
+                field = table.attrs["values_field_z"]
+                phys = table.attrs["values_physics"]
+                if field == 220.0 and phys=="standard_opt_4":
+                    energy = table.attrs["values_energy"]
+                    number = table.attrs["values_number"]
+                    data = table.read()
+                    indx_gamma = data["particle"] == 22
+                    gamma = data[indx_gamma]
+                    g_hig, g_low = get_values(gamma["energy"])
+                    e_hig, e_low = get_values(data[np.logical_not(indx_gamma)]["energy"])
+                    row["energy"] = energy
+                    row["number"] = number
+                    row["bins_low"] = bins_1
+                    row["bins_high"] = bins_2
+                    row["gamma_hist_low"] = g_low
+                    row["gamma_hist_high"] = g_hig
+                    row["electron_hist_low"] = e_low
+                    row["electron_hist_high"] = e_hig
+                    row.append()
+                    break
+        hist_table.flush()
+
 def process():
     with tables.open_file("hist_220_expacs.hd5") as fin:
         hist_table = fin.get_node("/", "histogram")
@@ -63,6 +92,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Process aragats EXAPCS.')
     parser.add_argument('--convert', action='store_true')
+    parser.add_argument('--add', action='store_true')
     parser.add_argument('--process', action='store_true')
 
     args = parser.parse_args()
@@ -71,6 +101,8 @@ def main():
         convert()
     elif args.process:
         process()
+    elif args.add:
+        add_1mev()
 
 if __name__ == '__main__':
     main()
